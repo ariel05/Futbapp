0284b64d7107bf6c5c83a78b6b4db81ab76b8936
EnzoYorki <enzoyorki@gmail.com>
2017-07-19 12:20:47 -0300

    Arreglo visual del código de Usuario y TestController

--- Futbapp/Controllers/TestController.cs
+++ Futbapp/Controllers/TestController.cs
@@ -15,18 +15,15 @@ namespace Futbapp.Controllers
         {
             using(FutbappContext futbappDB = new Models.FutbappContext())
             {
-                Usuario nuevoUsuario = new Usuario("Ariel", "Rivero",
-                "ariel200506@gmail.com", "ariel05", "123456789",
-                "Buenos Aires", "La Matanza", "Virrey Del Pino");
- //               using (SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["FutbappContext"].ConnectionString))
-  //              {
-  //                  using (SqlCommand sentencia = conexion.CreateCommand())
-   //                 {
-                        futbappDB.Usuario.Add(nuevoUsuario);
-                        futbappDB.SaveChanges();
-  //                  }
+                Usuario nuevoUsuario = new Usuario();
+                nuevoUsuario.NombreDeUsuario = "ariel05";
+                nuevoUsuario.Nombre = "Ariel";
+                nuevoUsuario.Apellido = "Rivero";
+                nuevoUsuario.Email = "ariel200506@gmail.com";
+                nuevoUsuario.Password = "123456789";
 
-  //              }
+                futbappDB.Usuario.Add(nuevoUsuario);
+                futbappDB.SaveChanges();
             }
             return "Todo Ok";
         }
--- Futbapp/Models/Usuario.cs
+++ Futbapp/Models/Usuario.cs
@@ -10,21 +10,22 @@ namespace Futbapp.Models
     public class Usuario
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public String NombreDeUsuario { get; set; }
         public String Nombre { get; set; }
         public String Apellido { get; set; }
         public String Email { get; set; }
-        public Ubicacion Ubicacion;
         public String Password { get; set; }
+        public Ubicacion Ubicacion;
 
-        public Usuario(String nombre, String apellido, String email, String nombreDeUsuario, String password, 
+        public Usuario() { }
+
+        public Usuario(String nombreDeUsuario, String nombre, String apellido, String email,  String password, 
             String provincia, String localidad, String zona)
         {
+            NombreDeUsuario = nombreDeUsuario;
             Nombre = nombre;
             Apellido = apellido;
             Email = email;
-            NombreDeUsuario = nombreDeUsuario;
             Password = password;
             Ubicacion = new Ubicacion(provincia, localidad, zona);
         }
