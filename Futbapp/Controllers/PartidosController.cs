b77ada8f9e583e7388a53e45cdc31f71cbdd4561
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-07-27 23:30:20 -0300

    Creo las los controladores vacios para Partidos y miPerfil; agrego la vista para MiPerfil y lo agrego al href

---
+++ Futbapp/Controllers/PartidosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Futbapp.Controllers
+{
+    public class PartidosController : Controller
+    {
+        // GET: Partidos
+        public ActionResult CrearPartidos()
+        {
+            return View();
+        }
+
+        public ActionResult BuscarPartidos()
+        {
+            return View();
+        }
+    }
+}
---
+++ Futbapp/Controllers/PerfilDeUsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Futbapp.Controllers
+{
+    public class PerfilDeUsuarioController : Controller
+    {
+        // GET: PerfilDeUsuario
+        public ActionResult MiPerfil()
+        {
+            return View();
+        }
+    }
+}
