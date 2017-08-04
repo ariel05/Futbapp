7610a6caf7714d12a72e1e27e596e5feaec3473d
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-08-03 23:57:32 -0300

    actualizacion del insert

--- SqlInsert/SanJusto&Virrey.sql
+++ SqlInsert/SanJusto&Virrey.sql
@@ -1,32 +1,32 @@
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('Marce16','Marcelo','Merele','merelemar@gmail.com',17,5,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados , Posicion, Ubicacion_Id)
+values ('Marce16','Marcelo','Merele','merelemar@gmail.com','123456',17,5,0, '1');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('RussoR','Randy','Ruso','ranruso@gmail.com',11,4,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('RussoR','Randy','Ruso','ranruso@gmail.com','123456',11,4,0, '1');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('TheStartShot','Marcos','McRonny','mcM@gmail.com',6,5,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('TheStartShot','Marcos','McRonny','mcM@gmail.com','123456',6,5,0, '1');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('WalkerT','Leonel','Garrafa','leomax@gmail.com',12,5,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('WalkerT','Leonel','Garrafa','leomax@gmail.com','123456',12,5,0, '1');
 
-insert into Usuarios(NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('León','Eduardo','León','leoned@gmail.com',5,5,0);
+insert into Usuario(NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('León','Eduardo','León','leoned@gmail.com','123456',5,5,0, '3');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('Richard10','Ricardo','Mendoza','mr10@gmail.com',7,5,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('Richard10','Ricardo','Mendoza','mr10@gmail.com','123456',7,5,0, '3');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('Leman7','Cristian','Leman','cr7@gmail.com',9,5,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('Leman7','Cristian','Leman','cr7@gmail.com','123456',9,5,0, '3');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('Luchito','Lautaro','Díaz','Ltdr@gmail.com',21,5,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('Luchito','Lautaro','Díaz','Ltdr@gmail.com','123456',21,5,0, '4');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('Roman10','Roman','Escudero','esr102005@gmail.com',17,5,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('Roman10','Roman','Escudero','esr102005@gmail.com','123456',17,5,0, '4');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('ariel05','Ariel','Rivero','ariel200506@gmail.com',6,2,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('ariel05','Ariel','Rivero','ariel200506@gmail.com', '123456', 6,2,0, '5');
 
-insert into Usuarios (NombreDeUsuario, Nombre, Apellido, Email, GolesHechos, PartidosJugados, Posicion)
-values ('Rock10','Rolando','Ramón','ramone@gmail.com',12,5,0);
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+values ('Rock10','Rolando','Ramón','ramone@gmail.com', '123456', 12,5,0 , '5');
--- SqlInsert/UbicacionLaMatanza.sql
+++ SqlInsert/UbicacionLaMatanza.sql
@@ -1,46 +1,46 @@
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('1','Buenos Aires','La Matanza','San Justo')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('2','Buenos Aires','La Matanza','Ramos Mejía')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('3','Buenos Aires','La Matanza','Lomas Del Mirador')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('4','Buenos Aires','La Matanza','Ciudad Evita')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('5','Buenos Aires','La Matanza','Ciudadela')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('6','Buenos Aires','La Matanza','Casanova')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('7','Buenos Aires','La Matanza','Laferrere')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('8','Buenos Aires','La Matanza','Rafaél Castillo')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('9','Buenos Aires','La Matanza','González Catán')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('10','Buenos Aires','La Matanza','Villa Luzuriaga')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('11','Buenos Aires','La Matanza','Aldo Bonzi')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('12','Buenos Aires','La Matanza','Villa Madero')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('13','Buenos Aires','La Matanza','Virrey Del Pino')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('14','Buenos Aires','La Matanza','La Tablada')
 
-insert into Ubicacions ("Id","Provincia","Ciudad","Zona") values
+insert into Ubicacion ("Id","Provincia","Ciudad","Zona") values
 						('15','Buenos Aires','La Matanza','Tapiales')
 
 
