183eec808d204fa9ad3b36b3a97bb412ddd2a4db
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-08-04 01:49:28 -0300

    Agrego sentencias sql

---
+++ SqlInsert/SalasInsert.sql
@@ -0,0 +1,44 @@
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('1', 'Fernando Gago', '1');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('2', 'El Potrero', '2');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('3', 'Los Andes Futbol 5', '3');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('4', 'Los Pibes De La Esquina', '4');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('5', 'El Gaucho Argentino', '5');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('6', 'Los Hermanos', '6');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('7', 'Los Tigres', '7');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('8', 'Castillos-5', '8');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('9', 'Diego Armando Maradona', '9');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('10', 'Los Goleadores', '10');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('11', 'Bonzi-Five', '11');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('12', 'El Bombazo', '12');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('13', 'La Foresta Club', '13');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('14', 'La Tablada', '14');
+
+insert into Sala (Id, Nombre, Ubicacion_Id) values
+('15', 'EL Negro', '15');
