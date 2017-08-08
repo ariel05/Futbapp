fbe72fe8ba307da158a43e02de08bb9c07973be7
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-08-07 23:44:28 -0300

    Mi perfil muestra el equipo y la ubicacion del usuario

--- Futbapp/Controllers/UsuarioController.cs
+++ Futbapp/Controllers/UsuarioController.cs
@@ -19,9 +19,12 @@ namespace Futbapp.Controllers
             Equipo equipo = FutbappDB.Equipos.FirstOrDefault(u => u.NombreDeEquipo == usuario.EquipoID);
 
 //            List<Usuario> user = FutbappDB.Usuarios.Include(u => u.Equipo.NombreDeEquipo == usuario.EquipoID).ToList();
-            List<Usuario> user = FutbappDB.Usuarios.OrderBy(a => a.Apellido).Where(a => a.EquipoID == equipo.NombreDeEquipo).ToList();
+            List<Usuario> users = FutbappDB.Usuarios.OrderBy(a => a.Apellido).Where(a => a.EquipoID == equipo.NombreDeEquipo).ToList();
+            Ubicacion ubicacion = FutbappDB.Ubicaciones.FirstOrDefault(u => u.Id == usuario.UbicacionID);
 
-            ViewBag.Usuario = user;
+            ViewBag.Ubicacion = ubicacion;
+            ViewBag.Usuario = users;
+            ViewBag.Equipo = equipo;
 
             return View(usuario);
         }
