662144d8366372880a2f5dd7da7e1948219d45ff
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-08-07 23:12:09 -0300

    Modifico los insert

--- SqlInsert/SanJusto&Virrey.sql
+++ SqlInsert/SanJusto&Virrey.sql
@@ -1,32 +1,32 @@
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados , Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados , Posicion, UbicacionID)
 values ('Marce16','Marcelo','Merele','merelemar@gmail.com','123456',17,5,0, '1');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('RussoR','Randy','Ruso','ranruso@gmail.com','123456',11,4,0, '1');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('TheStartShot','Marcos','McRonny','mcM@gmail.com','123456',6,5,0, '1');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('WalkerT','Leonel','Garrafa','leomax@gmail.com','123456',12,5,0, '1');
 
-insert into Usuario(NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario(NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('León','Eduardo','León','leoned@gmail.com','123456',5,5,0, '3');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('Richard10','Ricardo','Mendoza','mr10@gmail.com','123456',7,5,0, '3');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('Leman7','Cristian','Leman','cr7@gmail.com','123456',9,5,0, '3');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('Luchito','Lautaro','Díaz','Ltdr@gmail.com','123456',21,5,0, '4');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('Roman10','Roman','Escudero','esr102005@gmail.com','123456',17,5,0, '4');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('ariel05','Ariel','Rivero','ariel200506@gmail.com', '123456', 6,2,0, '5');
 
-insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, Ubicacion_Id)
+insert into Usuario (NombreDeUsuario, Nombre, Apellido, Email, Password, GolesHechos, PartidosJugados, Posicion, UbicacionID)
 values ('Rock10','Rolando','Ramón','ramone@gmail.com', '123456', 12,5,0 , '5');
