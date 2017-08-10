ccded5500c6f0ffb31acd3b484d5f5c34e8562df
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-08-10 20:34:09 -0300

    Modifico el insert de las salas

--- SqlInsert/SalasInsert.sql
+++ SqlInsert/SalasInsert.sql
@@ -1,44 +1,44 @@
-insert into Sala (Id, Nombre, UbicacionID) values
-('1', 'Fernando Gago', '1');
+insert into Sala (Nombre, UbicacionID) values
+('Fernando Gago', '1');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('2', 'El Potrero', '2');
+insert into Sala (Nombre, UbicacionID) values
+('El Potrero', '2');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('3', 'Los Andes Futbol 5', '3');
+insert into Sala (Nombre, UbicacionID) values
+('Los Andes Futbol 5', '3');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('4', 'Los Pibes De La Esquina', '4');
+insert into Sala (Nombre, UbicacionID) values
+('Los Pibes De La Esquina', '4');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('5', 'El Gaucho Argentino', '5');
+insert into Sala (Nombre, UbicacionID) values
+('El Gaucho Argentino', '5');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('6', 'Los Hermanos', '6');
+insert into Sala (Nombre, UbicacionID) values
+('Los Hermanos', '6');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('7', 'Los Tigres', '7');
+insert into Sala (Nombre, UbicacionID) values
+('Los Tigres', '7');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('8', 'Castillos-5', '8');
+insert into Sala (Nombre, UbicacionID) values
+('Castillos-5', '8');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('9', 'Diego Armando Maradona', '9');
+insert into Sala (Nombre, UbicacionID) values
+('Diego Armando Maradona', '9');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('10', 'Los Goleadores', '10');
+insert into Sala (Nombre, UbicacionID) values
+('Los Goleadores', '10');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('11', 'Bonzi-Five', '11');
+insert into Sala (Nombre, UbicacionID) values
+('Bonzi-Five', '11');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('12', 'El Bombazo', '12');
+insert into Sala (Nombre, UbicacionID) values
+('El Bombazo', '12');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('13', 'La Foresta Club', '13');
+insert into Sala (Nombre, UbicacionID) values
+('La Foresta Club', '13');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('14', 'La Tablada', '14');
+insert into Sala (Nombre, UbicacionID) values
+('La Tablada', '14');
 
-insert into Sala (Id, Nombre, UbicacionID) values
-('15', 'EL Negro', '15');
+insert into Sala (Nombre, UbicacionID) values
+('EL Negro', '15');
