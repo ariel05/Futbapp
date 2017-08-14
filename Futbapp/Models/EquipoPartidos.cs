17743cb3e25ff51597a98c043c1417bbfb821b43
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-08-14 16:04:52 -0300

    Cambio el tipo de dato, del id de equipo, de string a int

--- Futbapp/Models/EquipoPartidos.cs
+++ Futbapp/Models/EquipoPartidos.cs
@@ -10,7 +10,7 @@ namespace Futbapp.Models
     [Table("EquipoPartidos")]
     public class EquipoPartidos
     {
-        public int EquipoID { get; set;}
+        public string EquipoID { get; set;}
         public int PartidoID { get; set; }
         public DateTime FechaDeJuego { get; set; }
         [ForeignKey("EquipoID")]
