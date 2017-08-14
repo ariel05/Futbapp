95ec34e1b54562fdac97761307879bcb489e88f3
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-08-14 15:26:07 -0300

    Creo la clase para las clases que se relacionan N:N

---
+++ Futbapp/Models/EquipoPartidos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Futbapp.Models
+{
+    [Table("EquipoPartidos")]
+    public class EquipoPartidos
+    {
+        [Key]
+        public int EquipoID { get; set;}
+        public int PartidoID { get; set; }
+
+        [ForeignKey("EquipoID")]
+        public Equipo Equipo { get; set; }
+        [ForeignKey("PartidoID")]
+        public Partido Partido { get; set; }
+    }
+}
