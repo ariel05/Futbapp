b11ec2e78276bc7b4e2952769683dc8705df2aa5
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-08-14 15:30:49 -0300

    Arreglo los atributos de la clase

--- Futbapp/Models/EquipoPartidos.cs
+++ Futbapp/Models/EquipoPartidos.cs
@@ -10,10 +10,9 @@ namespace Futbapp.Models
     [Table("EquipoPartidos")]
     public class EquipoPartidos
     {
-        [Key]
         public int EquipoID { get; set;}
         public int PartidoID { get; set; }
-
+        public DateTime FechaDeJuego { get; set; }
         [ForeignKey("EquipoID")]
         public Equipo Equipo { get; set; }
         [ForeignKey("PartidoID")]
